5b8f0064f68287981d7816bf793e541253e40c69
evanlarsen <evan.larsen@gmail.com>
2016-04-03 21:32:08 -0700

    these didn't get included in last commit for some odd reason

--- Client/src/app.ts
+++ Client/src/app.ts
@@ -1,14 +1,7 @@
 import {Router, RouterConfiguration} from 'aurelia-router';
-import {autoinject} from 'aurelia-framework';
-import * as Hammer from 'hammerjs';
-import * as rx from 'rx';
 
-@autoinject
 export class App {
   router: Router;
-
-  constructor(private element: Element){
-  }
 
   configureRouter(config: RouterConfiguration, router: Router) {
     config.title = 'Forseti';
@@ -20,27 +13,5 @@ export class App {
     ]);
 
     this.router = router;
-  }
-
-  attached(){
-    var source = this.watchPan();
-    source.subscribe(
-      (x) => { console.log(x);},
-      (err) => { console.log('err:', err);},
-      () => { console.log('completed');}
-    );
-  }
-
-  watchPan(){
-    var recognizer = new Hammer.Pan({ direction: Hammer.DIRECTION_HORIZONTAL, threshold: 10});
-    let hammer = new Hammer(<HTMLElement>this.element, {});
-    hammer.add(recognizer);
-    return rx.Observable.create(observer => {
-      hammer.on('pan', ev => {
-        observer.onNext(ev);
-      });
-
-      return () => { hammer.off('off'); };
-    });
   }
 }
