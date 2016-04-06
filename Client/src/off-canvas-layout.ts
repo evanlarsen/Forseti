ebdb932a515cef6bd9e3684d069aa14a0095b069
evanlarsen <evan.larsen@gmail.com>
2016-04-05 21:06:12 -0700

    creating off canvas layout logic for panning frames

--- Client/src/off-canvas-layout.ts
+++ Client/src/off-canvas-layout.ts
@@ -2,89 +2,220 @@ import {autoinject} from 'aurelia-framework';
 import * as Hammer from 'hammerjs';
 import * as rx from 'rx';
 
+let activeFrameTag = 'js-in-view';
+let animationDuration = 500;
+
 @autoinject
 export class OffCanvasLayout{
-  container;
-  frames;
-  onPanInitValues;
+  private world: World;
+  private inputState: InputState;
 
   constructor(private element: Element){
+    this.world = new World();
+    this.inputState = new InputState();
   }
 
   attached(){
-    this.container = this.element.querySelector('.off-canvas-layout-container');
-    this.frames = this.element.querySelectorAll('.off-canvas-content');
-
     this.initializeContent();
 
     var source = this.watchPan();
-    source.subscribe(
-      this.onPan.bind(this),
-      (err) => { console.log('err:', err);},
-      () => { console.log('completed');}
-    );
+
+    source
+      .map(e => {
+        return { deltaX: e.deltaX, isFirst: e.isFirst, isFinal: e.isFinal };
+      })
+      .subscribe(
+        this.onPan.bind(this),
+        (err) => { console.log('err:', err);},
+        () => { console.log('completed');}
+      );
   }
 
   private initializeContent(){
-    let indexOfFrameInView = this.getIndexOfFrameInView();
+    let frames = this.element.querySelectorAll('.off-canvas-content');
 
-
-    this.foreachFrame((frame, i) => {
-      let framesFromView = i - indexOfFrameInView;
-      let translateX = 100 * framesFromView;
-      frame.style.transform = `translateX(${translateX}%)`;
-    });
-  }
-
-  private foreachFrame(action){
-      for (let i = 0, max = this.frames.length; i < max; i++){
-        action.call(this, this.frames[i], i, this.frames);
-      }
-  }
-
-  private onPan(event){
-    let deltaX = 100*event.deltaX / window.innerWidth;
-    if (!this.onPanInitValues){
-      this.onPanInitValues = this.getInitialValues();
+    for(let i = 0, max = frames.length; i < max; i++){
+      let frame = new Frame(<HTMLElement>frames[i], i)
+      this.world.frames.push(frame);
     }
 
-    this.foreachFrame((frame, i) => {
-      let framesFromView = i - this.onPanInitValues.indexOfFrameInView;
-      let translateX = (100 * framesFromView) + deltaX;
-      frame.style.transform = `translateX(${translateX}%)`;
-    });
+    this.world.resetFramesPositions();
+    this.world.draw();
+  }
+
+  private onPan(event : HammerInput){
+    this.inputState.deltaX = 100*event.deltaX / window.innerWidth;
+    this.inputState.isUserDoneSwipping = false;
+
+    if (!this.inputState.isUserSwipping){
+      window.requestAnimationFrame(this.panAnimationLoop);
+      this.inputState.isUserSwipping = true;
+    }
 
     if (event.isFinal){
-      this.onPanInitValues = undefined;
+      this.inputState.isUserSwipping = false;
+      this.inputState.isUserDoneSwipping = true;
     }
   }
 
-  private getInitialValues(){
-    return {
-      indexOfFrameInView: this.getIndexOfFrameInView()
-    };
-  }
+  private panAnimationLoop = this._panAnimationLoop.bind(this);
+  private _panAnimationLoop(timestamp?: number){
+    this.world.slideWorld(this.inputState.deltaX);
+    this.world.draw();
 
-  private getIndexOfFrameInView(){
-    let indexOfFrameInView = 0;
-    this.foreachFrame((frame, i) => {
-      if (frame.classList.contains('js-in-view')){
-        indexOfFrameInView = i;
+    if (this.inputState.isUserDoneSwipping){
+      if (this.inputState.deltaX > World.slideThreshold || this.inputState.deltaX < -World.slideThreshold){
+        window.requestAnimationFrame(this.slideToNewViewAnimationLoop);
       }
-    });
-    return indexOfFrameInView;
+      return;
+    }
+    window.requestAnimationFrame(this.panAnimationLoop);
   }
 
-  private watchPan(){
+  startTimestamp: number;
+  private slideToNewViewAnimationLoop = this._slideToNewViewAnimationLoop.bind(this);
+  private _slideToNewViewAnimationLoop(timestamp?:number){
+    if (!this.startTimestamp){
+      this.startTimestamp = timestamp;
+    }
+    let timeDelta = timestamp - this.startTimestamp;
+    let timeRange = 100 * timeDelta / animationDuration;
+    let direction: number;
+    if ((this.inputState.isSlidingRight && this.world.canSlideRight)
+      || (this.inputState.isSlidingLeft && !this.world.canSlideLeft)){
+      direction = 1;
+    } else if ((this.inputState.isSlidingRight && !this.world.canSlideRight)
+      || (this.inputState.isSlidingLeft && this.world.canSlideLeft)){
+      direction = -1;
+    } else {
+      throw 'Not sure what happened here but couldnt determine which direction to slide';
+    }
+
+    let deltaX = ((100 * direction) - this.inputState.deltaX) * timeRange;
+    this.world.slideWorld(deltaX);
+    this.world.draw();
+
+    if (deltaX >= 100 || deltaX <= -100){
+      this.startTimestamp = undefined;
+      this.world.setNewActiveView(direction);
+      return;
+    }
+    window.requestAnimationFrame(this.slideToNewViewAnimationLoop);
+  }
+
+  private watchPan() : rx.Observable<HammerInput>{
     let recognizer = new Hammer.Pan({ direction: Hammer.DIRECTION_HORIZONTAL, threshold: 10});
     let hammer = new Hammer(<HTMLElement>this.element, {});
     hammer.add(recognizer);
-    return rx.Observable.create(observer => {
+    return rx.Observable.create<HammerInput>(observer => {
       hammer.on('pan', ev => {
         observer.onNext(ev);
       });
 
       return () => { hammer.off('off'); };
     });
+  }
+}
+
+class InputState{
+  isUserSwipping: boolean;
+  deltaX: number;
+  isUserDoneSwipping: boolean;
+
+  get isSlidingRight(): boolean{
+    return this.deltaX > 0;
+  }
+
+  get isSlidingLeft(): boolean{
+    return this.deltaX < 0;
+  }
+}
+
+class World{
+  public static slideThreshold = 50;
+  public frames: Frame[];
+  constructor(){
+    this.frames = [];
+  }
+
+  public resetFramesPositions(){
+    let activeIndex = this.getIndexOfActiveFrame();
+    this.foreachFrame((frame, i) => {
+      frame.xCoordinate = (activeIndex - i) * 100;
+    });
+  }
+
+  public draw(){
+    this.foreachFrame((frame, i) => {
+      frame.draw();
+    });
+  }
+
+  public slideWorld(deltaX: number){
+    this.foreachFrame((frame, i) => {
+      let framesFromView = i - this.getIndexOfActiveFrame();
+      frame.xCoordinate = (100 * framesFromView) + deltaX;
+    });
+  }
+
+  get canSlideRight(): boolean{
+    return !this.frames[0].isActive;
+  }
+
+  get canSlideLeft(): boolean{
+    return !this.frames[this.frames.length - 1].isActive;
+  }
+
+  public setNewActiveView(direction: number){
+    let activeIndex = this.getIndexOfActiveFrame();
+    let newIndex = activeIndex + direction;
+    if (newIndex < 0 || newIndex >= this.frames.length){
+      throw "new active index of frames is out of bounds";
+    }
+    this.foreachFrame((frame, i) => {
+      frame.isActive = !!(i === newIndex);
+    });
+  }
+
+  private getIndexOfActiveFrame(){
+    for(let i = 0, max = this.frames.length; i < max; i++){
+      if (this.frames[i].htmlElement.classList.contains(activeFrameTag)){
+        return i;
+      }
+    }
+    throw `There are no frames marked with the css class '${activeFrameTag}'`
+     + ' which marks the div on the canvas. All other divs will be'
+     + ' rendered off canvas.';
+  }
+
+  private foreachFrame(action: (frame: Frame, i: number) => void){
+    for(let i = 0, max = this.frames.length; i < max; i++){
+      action.call(this.frames[i], i);
+    }
+  }
+}
+
+class Frame{
+  public xCoordinate: number
+
+  constructor(
+    public htmlElement: HTMLElement,
+    public index: number)
+  {}
+
+  get isActive(){
+    return this.htmlElement.classList.contains(activeFrameTag);
+  }
+
+  set isActive(isActive: boolean){
+    if (isActive){
+      this.htmlElement.classList.add(activeFrameTag);
+    } else {
+      this.htmlElement.classList.remove(activeFrameTag);
+    }
+  }
+
+  draw(){
+    this.htmlElement.style.transform = `translateX(${this.xCoordinate}%)`;
   }
 }
